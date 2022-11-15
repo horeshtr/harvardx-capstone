42b887b87e3441cda4f0ae137785a17adc4f75d2
Travis Horesh <horeshtr@gmail.com>
2022-11-14 23:12:33 -0500

    Model completed but not optimized. Use of log for global sales is unclear

--- harvardx_capstone_personal_games_trh.R
+++ harvardx_capstone_personal_games_trh.R
@@ -79,6 +79,7 @@ data_grouped <- data_clean %>%
             First_Released = min(Year_of_Release),
             Original_Genre = first(Genre),
             Original_Publisher = first(Publisher),
+            Original_Developer = first(Developer),
             Original_Rating = first(Rating),
             Critic_Score = round(weighted.mean(Critic_Score, (Critic_Count / sum(Critic_Count))), 0),
             Critic_Count = round(mean(Critic_Count), 0),
@@ -92,46 +93,51 @@ print(data_grouped, width = 1000)
 # --------------------------------------------------------------------- #
 
 # Count distinct values for each categorical column
-data_main_chr <- data_main[, sapply(data_main,is.character)]
-chr_col_index <- match(data_main_chr, data_main)
+data_group_chr <- data_grouped[, sapply(data_grouped , is.character)]
+chr_col_index <- match(data_group_chr, data_grouped)
 n_dist <- function(column){
-  n_distinct(data_main[,column])
+  n_distinct(data_grouped[,column])
 }
 num_dist_values <- sapply(chr_col_index, n_dist)
-num_dist_values <- matrix(num_dist_values, ncol = ncol(data_main_chr))
-colnames(num_dist_values) <- colnames(data_main_chr)
+num_dist_values <- matrix(num_dist_values, ncol = ncol(data_group_chr))
+colnames(num_dist_values) <- colnames(data_group_chr)
 num_dist_values <- as_tibble(num_dist_values)
 num_dist_values
 
 # Measure completeness of data in each column
-col_index <- c(1:ncol(data_main))
+col_index <- c(1:ncol(data_grouped))
 col_complete <- function(column){
-  (nrow(data_main) - sum(is.na(data_main[,column]))) / nrow(data_main)
+  (nrow(data_grouped) - sum(is.na(data_grouped[,column]))) / nrow(data_grouped)
 }
 column_integrity <- sapply(col_index, col_complete)
-column_integrity <- matrix(column_integrity, ncol = ncol(data_main))
-colnames(column_integrity) <- colnames(data_main)
+column_integrity <- matrix(column_integrity, ncol = ncol(data_grouped))
+colnames(column_integrity) <- colnames(data_grouped)
 column_integrity <- as_tibble(column_integrity)
 print(column_integrity, width = 1000)
 column_integrity[colSums(column_integrity) >= 0.9]
 
 # Measure correlation of all numeric columns
-data_main_num <- data_main[, sapply(data_main,is.double) | sapply(data_main,is.integer)]
-res <- cor(data_main_num, use = "complete.obs")
+data_group_num <- data_grouped[, sapply(data_grouped,is.double) | sapply(data_grouped,is.integer)]
+res <- cor(data_group_num, use = "complete.obs")
 round(res, 2)
 # Nothing appears to be highly correlated, but the most correlated variables are:
 #   n_platforms, Critic_Score, Critic_Count, and strangely, User_Count
 
 # Youngest and Oldest Release Years
-min(data_main$Year_of_Release)
-max(data_main$Year_of_Release)
+min(data_grouped$Year_of_Release)
+max(data_grouped$Year_of_Release)
 
 # Summary Stats on Global_Sales
-min(data_main$Global_Sales)
-max(data_main$Global_Sales)
-mean(data_main$Global_Sales)
-median(data_main$Global_Sales)
+min(data_grouped$Global_Sales)
+max(data_grouped$Global_Sales)
+mean(data_grouped$Global_Sales)
+median(data_grouped$Global_Sales)
 
+# Review counts in categorical variables
+data_grouped %>% count(Original_Rating)
+    # should drop or regroup K-A and RP, which have 1 record each
+
+data_grouped %>% count(Original_Publisher)
 
 #### Need to filter out strata with few points to avoid highly variable estimates ####
 ###   Essentially, performing the work of regularization? ###
@@ -174,44 +180,77 @@ print(sales_strata_dat, width = 1000)
 # ----------------------------------------------------------------------#
 
 # Distribution by Global Sales
-data_main %>% 
+data_grouped %>% 
   ggplot(aes(x = Global_Sales)) +
   geom_histogram()
 
+# Distribution by logarithmic Global Sales
+data_grouped %>% 
+  ggplot(aes(x = Global_Sales)) +
+  geom_histogram() +
+  scale_x_log10()
+
+# Distribution by Year of Release
+data_grouped %>% group_by(First_Released) %>% 
+  count() %>% ggplot() + 
+  geom_bar(aes(First_Released, n), stat = "identity", 
+           fill = "gray75") + theme(axis.text.x = element_text(angle = 90))
+
+# Sales by Platform
+data_grouped %>%
+  ggplot(aes(x = Platform, y = Global_Sales)) +
+  geom_col() +
+  theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5))
+    # Consider collapsing into major platforms
+
 
 # Sales by Genre
-data_main %>%
+data_grouped %>%
   ggplot(aes(x = Original_Genre, y = Global_Sales)) +
   geom_col() +
   theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5)) 
 
 
 # Sales by Rating
-data_main %>%
+data_grouped %>%
   ggplot(aes(x = Original_Rating, y = Global_Sales)) +
   geom_col() +
   theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=0.5)) 
 
 
 # scatter plot of Global_Sales vs. Critic_Score
-data_main %>%
-  ggplot(aes(x = Critic_Score, y = log(Global_Sales))) +
-  geom_point(alpha = 0.5) +
-  geom_smooth(method = "loess") +
-  theme_minimal() 
+line_colors <- c("Critic score" = "#20A387FF", "User score" = "#95D840FF")
+data_grouped %>%
+  ggplot() +
+  geom_smooth(aes(Critic_Score, Global_Sales, color = "Critic score")) + 
+  geom_smooth(aes(User_Score, Global_Sales, color = "User score")) +
+  labs(color = "") + xlab("Score") + ylab("Global sales") + 
+  scale_color_manual(values = line_colors)
 
 cor(data_grouped$Critic_Score, data_grouped$Global_Sales)
 cor.test(data_grouped$Critic_Score, data_grouped$Global_Sales)
 
 # scatter plot of Global_Sales vs. User_Score
-data_main %>%
+data_grouped %>%
   ggplot(aes(x = User_Score, y = log(Global_Sales))) +
   geom_point(alpha = 0.5) +
   geom_smooth(method = "loess") +
   theme_minimal() 
 
-cor(data_grouped$User_Score, data_grouped$Global_Sales)
-cor.test(data_grouped$User_Score, data_grouped$Global_Sales)
+# Examine top publishers
+top_10_pubs <- (data_grouped %>% group_by(Original_Publisher) %>%
+                     summarize(total_sales = sum(Global_Sales)) %>% arrange(desc(total_sales)) %>% 
+                     top_n(10) %>% distinct(Original_Publisher))$Original_Publisher
+
+# Examine top developers
+top_10_devs <- (data_grouped %>% group_by(Original_Developer) %>%
+                  summarize(total_sales = sum(Global_Sales)) %>% arrange(desc(total_sales)) %>% 
+                  top_n(10) %>% distinct(Original_Developer))$Original_Developer
+
+
+data_grouped <- data_grouped %>% 
+  mutate(top_pub = ifelse(Original_Publisher %in% top_10_pubs, TRUE, FALSE),
+         top_dev = ifelse(Original_Developer %in% top_10_devs, TRUE, FALSE))
 
 
 # --------------------------------------------------------------------- #
@@ -221,41 +260,27 @@ cor.test(data_grouped$User_Score, data_grouped$Global_Sales)
 # Validation set will be 10% of the data
 set.seed(1, sample.kind="Rounding") 
 val_index <- createDataPartition(y = data_grouped$Global_Sales, times = 1, p = 0.1, list = FALSE)
-data_main <- data_grouped[-val_index,]
-validation_temp <- data_grouped[val_index,]
+data_split <- data_grouped[-val_index,]
+validation <- data_grouped[val_index,]
 
 # Confirm items are in both the validation and main data sets
-# validation <- validation_temp %>%
-#   semi_join(data_grouped, by = "Name") #%>%
-  # semi_join(data_main, by = "Platform") %>% 
-  # semi_join(data_main, by = "Publisher") %>%
-  # semi_join(data_main, by = "Developer") %>%
-  # semi_join(data_main, by = "Genre") %>%
-
-#Add the rows removed from the test_set back into train_set
-# removed_val <- anti_join(validation_temp, validation)
-# data_main <- rbind(data_grouped, removed_val)
-
-# Remove unneeded objects
-#rm(val_index, validation_temp, removed_val)
+total_data_split <- rbind(data_split, validation)
+for (f in 1:length(names(total_data_split))) {
+  levels(data_split[, f]) <- levels(total_data_split[, f])
+}
 
 
 # Split data into training and test sets
 set.seed(1, sample.kind = "Rounding")
-test_index <- createDataPartition(y = data_main$Global_Sales, times = 1, p = 0.1, list = FALSE)
-train_set <- data_main[-test_index,]
-test_set_temp <- data_main[test_index,]
+test_index <- createDataPartition(y = data_split$Global_Sales, times = 1, p = 0.1, list = FALSE)
+train_set <- data_split[-test_index,]
+test_set <- data_split[test_index,]
 
 #Confirm items are in both the train and test sets
-# test_set <- test_set_temp %>%
-#   semi_join(train_set, by = "Name")
-
-#Add the rows removed from the test_set back into train_set and remove unneeded objects
-# removed_test <- anti_join(test_set_temp, test_set)
-# train_set <- rbind(train_set, removed_test)
-
-# Remove unneeded objects
-#rm(test_set_temp, test_index, removed_test)
+total_data_train <- rbind(train_set, test_set)
+for (f in 1:length(names(total_data_train))) {
+  levels(train_set[, f]) <- levels(total_data_train[, f])
+}
 
 
 # --------------------------------------------------------------------- #
@@ -266,50 +291,24 @@ test_set_temp <- data_main[test_index,]
 # Fit lm()
 ##########################
 # Fit a model based on numeric variables with highest correlation to Global Sales
-num_model_fit <- lm(formula = Global_Sales ~ n_platforms + Critic_Score + Critic_Count + User_Count, 
-                data = train_set, na.action = na.omit)
-summary(num_model_fit)
-
-# Investigate correlation between individual categorical variables and Global Sales
-# Platform
-platform_model_fit <- lm(formula = Global_Sales ~ Platform , data = train_set, na.action = na.omit)
-summary(platform_model_fit)
-(summary(platform_model_fit))$adj.r.squared
-
-# Original_Genre
-genre_model_fit <- lm(formula = Global_Sales ~ Original_Genre , 
-                         data = train_set, na.action = na.omit)
-summary(genre_model_fit)
-(summary(genre_model_fit))$adj.r.squared
-
-# Original_Publisher
-publisher_model_fit <- lm(formula = Global_Sales ~ Original_Publisher , 
-                         data = train_set, na.action = na.omit)
-summary(publisher_model_fit)
-(summary(publisher_model_fit))$adj.r.squared
-
-# Original_Rating
-rating_model_fit <- lm(formula = Global_Sales ~ Original_Rating , 
-                       data = train_set, na.action = na.omit)
-summary(rating_model_fit)
-(summary(rating_model_fit))$adj.r.squared
-
-
-# Fit a model that combines continuous and categorical variables most correlated to Global Sales
-model_fit <- lm(formula = Global_Sales ~ n_platforms + User_Count + Critic_Score + Critic_Count, 
-                data = train_set, na.action = na.omit)
+model_fit <- lm(formula = log(Global_Sales) ~ Critic_Score + User_Score + Original_Genre +
+                      First_Released + n_platforms + Critic_Count + User_Count + Original_Rating +
+                      top_pub + top_dev,data = train_set, na.action = na.omit)
 summary(model_fit)
 
 
-# --------------------------------------------------------------------- #
-#   Model development using RMSE 
-# ----------------------------------------------------------------------#
-
+##########################
+# RMSE Function
+##########################
 # Create the function for calculating Root Mean Squared Error (RMSE)
 RMSE <- function(true_sales, predicted_sales){ 
   sqrt(mean((true_sales - predicted_sales) ^ 2))
 }
 
+
+# --------------------------------------------------------------------- #
+#   Model development using RMSE 
+# ----------------------------------------------------------------------#
 
 # Overall average of Global_Sales in training set
 mu_train_sales <- mean(train_set$Global_Sales)
@@ -320,67 +319,8 @@ mu_train_sales
 # Naive Model
 ##########################
 # Rating difference between overall average of the train_set and true ratings of test_set
-naive_rmse <- RMSE(test_set_temp$Global_Sales, mu_train_sales)
+naive_rmse <- RMSE(test_set$Global_Sales, mu_train_sales)
 rmse_results <- tibble(Method = "Naive Model", RMSE = naive_rmse)
-
-
-##########################
-# Major Correlated Effects
-##########################
-
-# Critic_Count Effects
-critic_count_effects <- train_set %>%
-  group_by(Critic_Count) %>% 
-  summarize(b_cc = mean(Global_Sales - mu_train_sales))
-
-# n_platforms Effects
-n_platforms_effects <- train_set %>% 
-  left_join(critic_count_effects, by = "Critic_Count", na_matches = "never") %>%
-  group_by(n_platforms) %>% 
-  summarize(b_np = mean(Global_Sales - mu_train_sales - b_cc))
-
-# User_Count Effects
-user_count_effects <- train_set %>% 
-  left_join(critic_count_effects, by = "Critic_Count", na_matches = "never") %>%
-  left_join(n_platforms_effects, by = "n_platforms", na_matches = "never") %>%
-  group_by(User_Count) %>% 
-  summarize(b_uc = mean(Global_Sales - mu_train_sales - b_cc - b_np))
-
-# Critic_Score Effects
-critic_score_effects <- train_set %>% 
-  left_join(critic_count_effects, by = "Critic_Count", na_matches = "never") %>%
-  left_join(n_platforms_effects, by = "n_platforms", na_matches = "never") %>%
-  left_join(user_count_effects, by = "User_Count", na_matches = "never") %>%
-  group_by(Critic_Score) %>% 
-  summarize(b_cs = mean(Global_Sales - mu_train_sales - b_cc - b_np - b_uc))
-
-
-# 1) predictions using combined effects
-predicted_ratings_1 <- test_set_temp %>% 
-  left_join(critic_count_effects, by = "Critic_Count", na_matches = "never") %>%
-  left_join(n_platforms_effects, by = "n_platforms", na_matches = "never") %>%
-  left_join(user_count_effects, by = "User_Count", na_matches = "never") %>%
-  left_join(critic_score_effects, by = "Critic_Score", na_matches = "never") %>%
-  mutate(predictions = mu_train_sales + b_cc + b_np + b_uc + b_cs) %>%
-  pull(predictions)
-
-# check for missing values
-sum(is.na(predicted_ratings_1))
-predicted_ratings_1[is.na(predicted_ratings_1)==1]
-# Chose to replace NAs with the overall mean of Global_Sales
-predicted_ratings_1c <- ifelse(is.na(predicted_ratings_1), mu_train_sales, predicted_ratings_1)
-# Created an alternate predicted_ratings with all NAs replaced by 0
-predicted_ratings_2c <- ifelse(is.na(predicted_ratings_1), 0, predicted_ratings_1)
-
-# results for mu_train_sales
-combined_rmse <- RMSE(test_set_temp$Global_Sales, predicted_ratings_1c)
-rmse_results <- rmse_results %>% add_row(Method = "Combined Effects", RMSE = combined_rmse)
-
-# results for NAs removed
-combined_clean_rmse <- RMSE(test_set_temp$Global_Sales, predicted_ratings_2c)
-rmse_results <- rmse_results %>% 
-  add_row(Method = "Combined Effects No NAs", RMSE = combined_clean_rmse)
-
 
 
 ####################################
@@ -388,19 +328,20 @@ rmse_results <- rmse_results %>%
 ###################################
 
 # Test Set
-test_predict <- predict.lm(model_fit, test_set_temp)
+test_predict <- predict.lm(model_fit, test_set)
 summary(test_predict)
 
-test_set_rmse <- RMSE(test_set_temp$Global_Sales, test_predict)
+test_set_rmse <- RMSE(log(test_set$Global_Sales), test_predict)
 rmse_results <- rmse_results %>% add_row(Method = "lm_fit_test", RMSE = test_set_rmse)
 
 # Validation Set
-val_predict <- predict.lm(model_fit, validation_temp)
+val_predict <- predict.lm(model_fit, validation)
 summary(val_predict)
 
-val_set_rmse <- RMSE(validation_temp$Global_Sales, val_predict)
+val_set_rmse <- RMSE(log(validation$Global_Sales), val_predict)
 rmse_results <- rmse_results %>% add_row(Method = "lm_fit_val", RMSE = val_set_rmse)
 
+## log(Global_Sales) versus regular? 
 
 ###############################################
 # Results Table
